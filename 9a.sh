e49e7ff65249c13878ed6a4097599637f1f43698
rithugowda <98948570+rithugowda@users.noreply.github.com>
2022-05-15 12:31:32 +0530

    Update 9a.sh

--- 9a.sh
+++ 9a.sh
@@ -1,17 +1,29 @@
 if [ $# -eq 3 ]
 then
+	if [ -e $1 ] 
+	then
 	if [ -f $1 ]
 	then
-		l=`wc -l $1 | cut -d " " -f 1`
-		if [ $2 -le $l -a $3 -le $l -a $2 -le $3 -a $2 -gt 0 -a $3 -gt 0 ]
+	
+		if [ $2 -gt 0 -a $3 -gt 0 ]
 		then
-			head -n $3 $1 | tail -n +$2
+			l=`wc -l $1 | cut -d " " -f 1`
+			if [ $2 -le $l -a $3 -le $l -a $3 -ge $2 ]
+			then
+				head -n $3 $1 | tail -n +$2
+			else
+				echo $2 and $3 should be less than total line and Ending line should be greater than Starting line
+			fi
 		else
-			echo "Enter valid file "
+			echo The $2 and $3 must be greater than zero 
+		fi
+		else
+		echo It is not a file
 		fi
 	else
-		echo "Enter file name proper"
+		echo Filename: $1 does not exist
 	fi
+
 else
-	echo "Usage:FileName StartingLine and EndLine"
+	echo Enter Filename, Starting and ending line numbers
 fi
