eebf3942d1edfaef0756ac3b999dff07c596a178
rithugowda <98948570+rithugowda@users.noreply.github.com>
2022-05-16 14:45:35 +0530

    Update 2b.sh

--- 2b.sh
+++ 2b.sh
@@ -1,14 +1,14 @@
 if [ $# -eq 1 ]
 then
-grep $1 /etc/passwd >ud
-if [ $? -eq 0 ]
-then
-echo "user exist"
-h=`cut -d ":" -f 6 ud`
-echo "home directory of $1 and $h"
+	grep $1 /etc/passwd >ud
+	if [ $? -eq 0 ]
+	then
+		echo "user exist"
+		h=`cut -d ":" -f 6 ud`
+		echo "home directory of $1 and $h"
+	else
+		echo " not user directory"
+	fi
 else
-echo " not user directory"
-fi
-else
-echo " enter user name"
+	echo " enter user name"
 fi
