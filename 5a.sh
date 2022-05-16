19087adc01356ab84e05e071f53a839d70d5fbf3
rithugowda <98948570+rithugowda@users.noreply.github.com>
2022-05-16 14:54:41 +0530

    Update 5a.sh

--- 5a.sh
+++ 5a.sh
@@ -3,9 +3,8 @@
 fil=$1
 if [ -e $fil ]
 then
-set -- `ls -ld $fil`
-echo " the file modification is on $6 $7 at $8"
+	set -- `ls -ld $fil`
+	echo " the file modification is on $6 $7 at $8"
 else
-echo "file doesnot exit"
+	echo "file doesnot exit"
 fi
-
