7c0ce0600809cd154966dc520f16c59b5cdfa011
rithugowda <98948570+rithugowda@users.noreply.github.com>
2022-05-16 14:57:40 +0530

    Update 7a.sh

--- 7a.sh
+++ 7a.sh
@@ -4,28 +4,28 @@ u=`whoami`
 set -- `who`
 for i in $*
 do
-user=$1
-if [ $u =  $user ]
-then
-set -- `echo $4 | tr ":" " "`
-h=$1
-t=`date +%H`
-echo $t
-if [ $h -ge 4 ] && [ $h -lt 12 ]
-then
-echo " good mrng mr $u "
-elif [ $h -ge 12 ] && [ $h - lt 16 ]
-then
-echo " good aftrnoon mr $u "
-elif [ $h -ge 16 ] && [ $h - lt 19 ]
-then
-echo " good evng mr $u "
-else
+	user=$1
+	if [ $u =  $user ]
+	then
+		set -- `echo $4 | tr ":" " "`
+		h=$1
+		t=`date +%H`
+		echo $t
+		if [ $h -ge 4 ] && [ $h -lt 12 ]
+		then
+			echo " good mrng mr $u "
+		elif [ $h -ge 12 ] && [ $h - lt 16 ]
+		then
+			echo " good aftrnoon mr $u "
+		elif [ $h -ge 16 ] && [ $h - lt 19 ]
+		then
+			echo " good evng mr $u "
+		else
 
-echo " good night mr $u "
-fi
-exit
-else
-shift 5
-fi
+			echo " good night mr $u "
+		fi
+		exit
+	else
+		shift 5
+	fi
 done
