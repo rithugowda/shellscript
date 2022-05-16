1db435d1942eedb4dbc318cc3252c2f0070d4e5b
rithugowda <98948570+rithugowda@users.noreply.github.com>
2022-05-16 14:42:12 +0530

    Update 1b.sh

--- 1b.sh
+++ 1b.sh
@@ -1,13 +1,12 @@
 if [ $# -ne 0 ]
- then
-     p=`echo $1 | tr "/" " "`
-for i  in $p
-    do
-      mkdir $i
-        cd $i
-done
-echo "All directories created"
+then
+	p=`echo $1 | tr "/" " "`
+	for i  in $p
+	do
+		mkdir $i
+		cd $i
+	done
+	echo "All directories created"
 else
-echo "please enter the permission"
+	echo "please enter the permission"
 fi
-
