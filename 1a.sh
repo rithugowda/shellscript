6ab3a1c417df21b66442c37cc8eb04849e917cc7
rithugowda <98948570+rithugowda@users.noreply.github.com>
2022-05-16 14:40:57 +0530

    Update 1a.sh

--- 1a.sh
+++ 1a.sh
@@ -1,13 +1,13 @@
 if [ $# -eq 1 ]
-  then
-if [ -d $1 ]
-  then
-      set -- `ls -Rl $1 | grep "^-" | tr -s " " | cut -d " " -f 5,9- | sort -n | tail -n 1`
-      echo "file size: $1"
-      echo " name fil $2"
+then
+	if [ -d $1 ]
+	then
+		set -- `ls -Rl $1 | grep "^-" | tr -s " " | cut -d " " -f 5,9- | sort -n | tail -n 1`
+		echo "file size: $1"
+		echo " name fil $2"
+	else
+		echo "not a directory"
+	fi
 else
-      echo "not a directory"
-fi
-      else
-      echo " please  entr directory name"
+	echo " please  entr directory name"
 fi
