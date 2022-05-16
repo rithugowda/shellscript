404f97d112bd459cf51f72600f9cf503f1c49b77
rithugowda <98948570+rithugowda@users.noreply.github.com>
2022-05-16 16:46:48 +0530

    Update 8a.sh

--- 8a.sh
+++ 8a.sh
@@ -1,6 +1,6 @@
-echo -e "enter the user name :\c"
+echo "enter the user name "
 read usr
-tuser=`who | tr -s " " | head -1 | cut -d " " -f1`
+tuser=`whoami`
 if [ "$tuser" = "$usr" ]
 then
 	tm=`who | tr -s " " | head -1 | cut -d " " -f4`
@@ -8,11 +8,7 @@ then
 	umin=`echo $tm | cut -d ":" -f2`
 	shr=`date "+%H"`
 	smin=`date "+%M"`
-	if [ $smin -lt $umin ]
-	then
-		shr=`expr $shr - 1`
-		smin=`expr $smin + 60`
-	fi
+	
 	h=`expr $shr - $uhr`
 	m=`expr $smin - $umin`
 	echo "user name : $usr"
