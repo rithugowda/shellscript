3e1b500f0c08dba9ccc45e8e0da4b3cd7a0b0c40
rithugowda <98948570+rithugowda@users.noreply.github.com>
2022-05-16 14:55:34 +0530

    Update 5b.sh

--- 5b.sh
+++ 5b.sh
@@ -3,8 +3,7 @@ d=`expr $d + 0`
 h=`cal | tr -d "[\137][\010]" | grep -wn "$d" | head -c 1`
 if [ $d -gt 9 ]
 then
-cal | tr -d "[\137][\010]" | sed  " $h s/$d/**/"
+	cal | tr -d "[\137][\010]" | sed  " $h s/$d/**/"
 else
-cal | tr -d "[\137][\010]" | sed  " $h s/$d/*/"
+	cal | tr -d "[\137][\010]" | sed  " $h s/$d/*/"
 fi
-
