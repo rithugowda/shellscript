3f5bf6f670d58c07061dadaed0c373c889ad3e03
rithugowda <98948570+rithugowda@users.noreply.github.com>
2022-05-25 19:19:23 +0530

    changed

--- 6a.sh
+++ 6a.sh
@@ -1,31 +1,29 @@
 
 if [ $# -ne 0 ]
- then
- if [ -e $* ]
- then
- if [ -f $* ]
- then
- 
- for i in $*
- do
- 
-if [ -e ~/mydir ]
 then
- ls $*
- cat $*
- 
- cp  $* ~/mydir
- else
- echo "Mydir is  not exits to do copy"
- fi
- done
- else
- echo " it is not a file "
- fi
- else
- echo " file does not exit"
- fi
- else
- echo “please enter arguments”
- 
+
+
+	for i in $*
+	do
+
+		if [ -e ~/mydir ]
+		then
+
+
+			if [ `ls ~/mydir | grep $i* | wc -l` -eq 0 ]
+			then
+				ls $i*
+				cat $i*
+				cp  $* ~/mydir
+			else
+				echo cant copy  $i is already exit in mydir
+			fi
+		else
+			echo "Mydir is  not exits to do copy"
+		fi
+	done
+else
+
+	echo “please enter arguments”
+
 fi
