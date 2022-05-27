01309685043628c37c5012fa9eb0ce814e558aea
rithugowda <98948570+rithugowda@users.noreply.github.com>
2022-05-27 22:08:01 +0530

    updated

--- 6a.sh
+++ 6a.sh
@@ -1,62 +1,30 @@
 if [ $# -ne 0 ]
 then
-  
-  
-
-	 for i in $*
-do
-
-		if [ -e ~/mydir ]
-		then
-                         
-
+	if [ ! -e ~/mydir ]
+	then
+		mkdir ~/mydir
+	fi
+	if [ -e $1 ]
+	then
+		for i in $*
+		do
 			if [ `ls ~/mydir | grep $i* | wc -l` -ne 0 ]
 			then
-			echo file $i already exist
-			
+				echo file $i already exist
 			else
-			
-			while [ `ls ~/mydir | grep $i* | wc -l` -eq 0 ]
-			
-		             do
-	
-				ls $*
-				cat $*
-				cp  $* ~/mydir
-			
-			
-			done
-			break
-			
-			fi
-		else
-			mkdir ~/mydir
-			if [ `ls ~/mydir | grep $i* | wc -l` -ne 0 ]
-			then
-			echo file $i already exist
-			
-			else
-			
-			while [ `ls ~/mydir | grep $i* | wc -l` -eq 0 ]
-			
-		             do
-	
-				ls $*
-				cat $*
-				cp  $* ~/mydir
-			
-			
-			done
-			break
-			
-			fi
-		fi
-		
+
+				while [ `ls ~/mydir | grep $i* | wc -l` -eq 0 ]
+				do
+					ls $*
+					cat $*
+					cp  $* ~/mydir					
+				done
+				break
+			fi		
 		done
-		
-		
+	else
+		echo no such file
+	fi
 else
-
 	echo “please enter arguments”
-
 fi
